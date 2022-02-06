8c9f28572df0f04a7ee1f5c966fe6911c6a96720
advaith <adv20oct2000@gmail.com>
2022-02-06 18:06:40 +0530

    added latest themeprovider

--- src/App.js
+++ src/App.js
@@ -26,19 +26,9 @@ import LocalizationProvider from "@mui/lab/LocalizationProvider";
 
 import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
 
-import {
-  ThemeProvider,
-  createMuiTheme,
-  makeStyles,
-} from "@material-ui/core/styles";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
 
-const theme = createMuiTheme();
-
-const useStyles = makeStyles((theme) => {
-  root: {
-    // some CSS that access to theme
-  }
-});
+const theme = createTheme({});
 
 function App() {
   return (
